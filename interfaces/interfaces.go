3da3a5fbc6e81eeb75d18cb7b9c57dc8c0483bee
Golden Kumar <golden.k@appointy.com>
2025-05-28 19:15:34 +0530

    wip: starting core from interface.yaml

--- interfaces/interfaces.go
+++ interfaces/interfaces.go
@@ -1,13 +1,5 @@
 package interfaces
 
-import (
-	"os"
-	"path"
-	"path/filepath"
-
-	"gopkg.in/yaml.v3"
-)
-
 type PluginID string
 
 type Deployment string
@@ -17,30 +9,9 @@ const (
 	MONOLITHIC   Deployment = "monolithic"
 )
 
-type PluginConfig struct {
-	Plugins []PluginData `yaml:"plugins"`
-}
-
 type PluginData struct {
 	Name       string `yaml:"name"`
 	Interface  string `yaml:"interface"`
 	Deployment string `yaml:"deployment"`
 	Source     string `yaml:"source,omitempty"`
-}
-
-func ParsePluginYaml(src string) *PluginConfig {
-	currDir, _ := filepath.Abs(".")
-	filePath := path.Join(currDir, src)
-
-	data, err := os.ReadFile(filePath)
-	if err != nil {
-		panic(err)
-	}
-
-	result := PluginConfig{}
-	if err := yaml.Unmarshal(data, &result); err != nil {
-		panic(err)
-	}
-
-	return &result
 }
--- main.go
+++ main.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"log"
+	"os"
+	"path"
+	"path/filepath"
 	"saastack/core"
 	emailv1 "saastack/gen/email/v1"
 	paymentv1 "saastack/gen/payment/v1"
@@ -11,79 +15,148 @@ import (
 	paymentType "saastack/interfaces/payment/types"
 	"saastack/plugins/email"
 	"saastack/plugins/payment"
+
+	"gopkg.in/yaml.v2"
 )
 
-func RegisterEmailPlugin() {
-	mailGun := email.NewMailGun()
-	emailData := emailType.PluginMapData{
-		Plugin: interfaces.PluginData{
-			Name:       "mailgun",
-			Deployment: string(interfaces.MONOLITHIC),
-		},
-		Client: mailGun,
-	}
-	emailService.RegisterNewEmailPlugin(emailData)
-	awsses := email.NewAmazonSES()
-	emailData = emailType.PluginMapData{
-		Plugin: interfaces.PluginData{
-			Name:       "awsses",
-			Deployment: string(interfaces.MONOLITHIC),
-		},
-		Client: awsses,
-	}
-	emailService.RegisterNewEmailPlugin(emailData)
+var Services map[string]bool = make(map[string]bool)
 
-	emailData = emailType.PluginMapData{
-		Plugin: interfaces.PluginData{
-			Name:       "custom",
-			Deployment: string(interfaces.MICROSERVICE),
-			Source:     "localhost:9002",
-		},
-	}
-	emailService.RegisterNewEmailPlugin(emailData)
-}
+func init() {
+	src := "interface.yaml"
+	currDir, _ := filepath.Abs(".")
+	src = path.Join(currDir, src)
 
-func RegisterPaymentPlugin() {
-	stripe := payment.NewStripeClient()
-	payData := paymentType.PluginMapData{
-		Plugin: interfaces.PluginData{
-			Name:       "stripe",
-			Deployment: string(interfaces.MONOLITHIC),
-		},
-		Client: stripe,
-	}
-	paymentService.RegisterNewPaymentPlugin(payData)
+	res := ParsePluginYaml(src)
 
-	razorpay := payment.NewRazorPayClient()
-	payData = paymentType.PluginMapData{
-		Plugin: interfaces.PluginData{
-			Name:       "razorpay",
-			Deployment: string(interfaces.MONOLITHIC),
-		},
-		Client: razorpay,
+	for _, val := range res.Services {
+		Services[val] = true
 	}
-	paymentService.RegisterNewPaymentPlugin(payData)
 
-	payData = paymentType.PluginMapData{
-		Plugin: interfaces.PluginData{
-			Name:       "custom",
-			Deployment: string(interfaces.MICROSERVICE),
-			Source:     "localhost:9003",
-		},
+	for _, config := range res.Plugins {
+		_, ok := Services[config.Interface]
+		if !ok {
+			log.Println("Service not found")
+			continue
+		}
+
+		switch config.Interface {
+		case "email":
+			RegisterEmailPlugin(config)
+		case "payment":
+			RegisterPaymentPlugin(config)
+		default:
+			log.Println("Interface not implemented", config.Interface)
+		}
 	}
-	paymentService.RegisterNewPaymentPlugin(payData)
+
+	log.Println(res)
 }
 
 func main() {
 	srv := core.NewGrpcServer()
 
-	RegisterPaymentPlugin()
-	RegisterEmailPlugin()
-
-	emailv1.RegisterEmailServiceServer(srv, &emailService.EmailService{})
-	paymentv1.RegisterPaymentServiceServer(srv, &paymentService.PaymentService{})
+	// Register Service to core
+	for key := range Services {
+		switch key {
+		case "email":
+			emailv1.RegisterEmailServiceServer(srv, &emailService.EmailService{})
+		case "payment":
+			paymentv1.RegisterPaymentServiceServer(srv, &paymentService.PaymentService{})
+		default:
+			log.Println("Interface Handler Not Implemented")
+		}
+	}
 
 	if err := core.StartServer(srv); err != nil {
 		panic(err)
 	}
+}
+
+type PluginConfig struct {
+	Plugins  []interfaces.PluginData `yaml:"plugins"`
+	Services []string                `yaml:"services"`
+}
+
+func ParsePluginYaml(src string) *PluginConfig {
+	data, err := os.ReadFile(src)
+	if err != nil {
+		panic(err)
+	}
+
+	result := PluginConfig{}
+	if err := yaml.Unmarshal(data, &result); err != nil {
+		panic(err)
+	}
+
+	return &result
+}
+
+func RegisterEmailPlugin(config interfaces.PluginData) {
+	var data emailType.PluginMapData
+
+	if config.Deployment == string(interfaces.MICROSERVICE) {
+		data = emailType.PluginMapData{
+			Plugin: interfaces.PluginData{
+				Name:       config.Name,
+				Deployment: config.Deployment,
+				Source:     config.Source,
+			},
+		}
+	} else if config.Deployment == string(interfaces.MONOLITHIC) {
+		var client emailType.EmailPlugin
+
+		switch config.Name {
+		case "mailgun":
+			client = email.NewMailGun()
+		case "awsses":
+			client = email.NewAmazonSES()
+		default:
+			log.Println("Plugin Instance Not Implemented")
+			return
+		}
+		data = emailType.PluginMapData{
+			Plugin: interfaces.PluginData{
+				Name:       config.Name,
+				Deployment: config.Deployment,
+				Source:     config.Source,
+			},
+			Client: client,
+		}
+	}
+	emailService.RegisterNewEmailPlugin(data)
+}
+
+func RegisterPaymentPlugin(config interfaces.PluginData) {
+	var data paymentType.PluginMapData
+
+	if config.Deployment == string(interfaces.MICROSERVICE) {
+		data = paymentType.PluginMapData{
+			Plugin: interfaces.PluginData{
+				Name:       config.Name,
+				Deployment: config.Deployment,
+				Source:     config.Source,
+			},
+		}
+	} else if config.Deployment == string(interfaces.MONOLITHIC) {
+		var client paymentType.PaymentPlugin
+
+		switch config.Name {
+		case "stripe":
+			client = payment.NewStripeClient()
+		case "razorpay":
+			client = payment.NewRazorPayClient()
+		default:
+			log.Println("Plugin Instance Not Implemented")
+			return
+		}
+		data = paymentType.PluginMapData{
+			Plugin: interfaces.PluginData{
+				Name:       config.Name,
+				Deployment: config.Deployment,
+				Source:     config.Source,
+			},
+			Client: client,
+		}
+	}
+	paymentService.RegisterNewPaymentPlugin(data)
 }
